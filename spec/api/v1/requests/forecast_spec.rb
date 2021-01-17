5202bd48acf31edf1fa60a68b4286dcc4c469244
Kate Tester <katemorris@gmail.com>
2021-01-16 22:06:54 -0700

    Changes test to use VCR

--- spec/api/v1/requests/forecast_spec.rb
+++ spec/api/v1/requests/forecast_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'GET /api/v1/forecast?location=X', type: :request do
   it 'given a city, state, should return forecast data' do
-    location = 'Killeen, TX'
-    get api_v1_forecast_path, params: { location: location }
+    VCR.use_cassette('killeen_forecast') do
+      location = 'Killeen, TX'
+      get api_v1_forecast_path, params: { location: location }
 
-    expect(response.status).to eq(200)
-    forecast = JSON.parse(response.body, symbolize_names: true)
-    forecast_get_response(forecast)
+      expect(response.status).to eq(200)
+      forecast = JSON.parse(response.body, symbolize_names: true)
+      forecast_get_response(forecast)
+    end
   end
 end
