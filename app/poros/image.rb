3cad17ce9f137c1c1c08012451f0c6713b674d6b
Kate Tester <katemorris@gmail.com>
2021-01-17 14:40:03 -0700

    Adds image poro for serializer

---
+++ app/poros/image.rb
@@ -0,0 +1,14 @@
+class Image
+  attr_reader :photo_url, :username, :user_url, :location, :id
+  def initialize(data, user, search)
+    @photo_url = make_url(data[:server], data[:id], data[:secret])
+    @username = user[:username][:_content]
+    @user_url = user[:photosurl][:_content]
+    @location = search
+    @id = 'null'
+  end
+
+  def make_url(server, id, secret)
+    "https://live.staticflickr.com/#{server}/#{id}_#{secret}.jpg"
+  end
+end
