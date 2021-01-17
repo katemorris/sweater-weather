3dfd69450a1a1c1cea31a9b5cec2b68fd947d99f
Kate Tester <katemorris@gmail.com>
2021-01-16 21:09:02 -0700

    Adds instance method test

--- spec/poros/daily_weather_spec.rb
+++ spec/poros/daily_weather_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe DailyWeather do
-  it "that it exists" do
-    data = {
+  before :each do
+    @data = {
       :dt=>1610820000,
       :sunrise=>1610803769,
       :sunset=>1610841102,
@@ -21,13 +21,23 @@ RSpec.describe DailyWeather do
       :uvi=>3.52
     }
 
-    dw = DailyWeather.new(data)
-    expect(dw.date).to eq("2021-01-16")
-    expect(dw.sunrise).to eq("Sat, 16 Jan 2021 06:29:29 -0700")
-    expect(dw.sunset).to eq("Sat, 16 Jan 2021 16:51:42 -0700")
-    expect(dw.max_temp).to eq(data[:temp][:max])
-    expect(dw.min_temp).to eq(data[:temp][:min])
-    expect(dw.conditions).to eq(data[:weather].first[:description])
-    expect(dw.icon).to eq(data[:weather].first[:icon])
+    @dw = DailyWeather.new(@data)
+  end
+
+  it "that it exists" do
+    expect(@dw.date).to eq("2021-01-16")
+    expect(@dw.sunrise).to eq("Sat, 16 Jan 2021 06:29:29 -0700")
+    expect(@dw.sunset).to eq("Sat, 16 Jan 2021 16:51:42 -0700")
+    expect(@dw.max_temp).to eq(@data[:temp][:max])
+    expect(@dw.min_temp).to eq(@data[:temp][:min])
+    expect(@dw.conditions).to eq(@data[:weather].first[:description])
+    expect(@dw.icon).to eq(@data[:weather].first[:icon])
+  end
+
+  describe 'instance methods' do
+    it '#date_conversion()' do
+      data = 1610820000
+      expect(@dw.date_conversion(data)).to eq("2021-01-16")
+    end
   end
 end
