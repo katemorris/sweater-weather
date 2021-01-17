cc9164e59efda18f85bc93c3e38ba5e409b7c2ee
Kate Tester <katemorris@gmail.com>
2021-01-17 14:39:51 -0700

    Updates test with current values in response

--- spec/api/v1/requests/image_spec.rb
+++ spec/api/v1/requests/image_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'GET /api/v1/backgrounds?location=X', type: :request do
-  xit 'given a city, state, should return an image' do
+  it 'given a city, state, should return an image' do
     VCR.use_cassette('killeen_image') do
       location = 'Killeen, TX'
-      get api_v1_background_path, params: { location: location }
+      get api_v1_backgrounds_path, params: { location: location }
 
       expect(response.status).to eq(200)
       image = JSON.parse(response.body, symbolize_names: true)
       expect(image[:data][:attributes][:image][:image_url]).to be_a String
       expect(image[:data][:attributes][:image][:location]).to eq(location)
       expect(image[:data][:attributes][:image]).to have_key(:credit)
+      expect(image[:data][:attributes][:image][:credit]).to have_key(:author)
+      expect(image[:data][:attributes][:image][:credit]).to have_key(:url)
     end
   end
 end
