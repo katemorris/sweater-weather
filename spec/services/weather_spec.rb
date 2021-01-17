0a09af5ed190c57681538b960144c9102993c844
Kate Tester <katemorris@gmail.com>
2021-01-16 20:13:55 -0700

    Adds test for weather service

---
+++ spec/services/weather_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe WeatherService do
+  describe 'class methods' do
+    it '.get_games_list_by_name()' do
+      VCR.use_cassette('sample_forecast') do
+        coords = {:lat=>31.1178,:lng=>-97.7311}
+        response = WeatherService.forecast(coords)
+
+        expect(response).to be_a Hash
+
+        expect(response).to be_a Hash
+        expect(response).to have_key(:current)
+        expect(response).to have_key(:hourly)
+        expect(response).to have_key(:daily)
+        expect(response).to_not have_key(:alerts)
+      end
+    end
+  end
+end
