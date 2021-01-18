508669bef0d783e80e1477436671f01de1fa56b5
Kate Tester <katemorris@gmail.com>
2021-01-17 18:47:19 -0700

    Adds comma in data table

--- spec/api/v1/requests/users_spec.rb
+++ spec/api/v1/requests/users_spec.rb
@@ -5,15 +5,15 @@ require 'rails_helper'
 RSpec.describe 'POST /api/v1/users', type: :request do
   it 'should register a user' do
     data = {
-      "email": "whatever@example.com",
-      "password": "password",
-      "password_confirmation": "password"
+      "email": 'whatever@example.com',
+      "password": 'password',
+      "password_confirmation": 'password'
     }
     post api_v1_users_path, params: data, as: :json
 
     expect(response.status).to eq(201)
     user = JSON.parse(response.body, symbolize_names: true)
 
-    register_user_checker(user, data["email"])
+    register_user_checker(user, data['email'])
   end
 end
