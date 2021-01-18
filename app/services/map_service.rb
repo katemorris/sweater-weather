c53eaa991fb49ad0521424cb43e8440f94c74f1f
Kate Tester <katemorris@gmail.com>
2021-01-17 20:46:20 -0700

    Breaks out connection and adds travel time method

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -2,8 +2,24 @@
 
 class MapService
   def self.coordinates(location)
-    conn = Faraday.new('https://www.mapquestapi.com')
-    response = conn.get("/geocoding/v1/address?key=#{ENV['MQ_KEY']}&location=#{location}")
+    response = conn.get("/geocoding/v1/address") do |req|
+      req.params['key'] = ENV['MQ_KEY']
+      req.params['location'] = location
+    end
     JSON.parse(response.body, symbolize_names: true)[:results].first[:locations].first[:displayLatLng]
+  end
+
+  def self.travel_time(origin, destination)
+    response = conn.get("/directions/v2/route") do |req|
+      req.params['key'] = ENV['MQ_KEY']
+      req.params['from'] = origin
+      req.params['to'] = destination
+      req.params['narrativeType'] = 'none'
+    end
+    JSON.parse(response.body, symbolize_names: true)[:route][:formattedTime]
+  end
+
+  def self.conn
+    Faraday.new('https://www.mapquestapi.com')
   end
 end
