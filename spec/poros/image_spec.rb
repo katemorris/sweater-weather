6d0abc804d09f6f5feebc58319a4ee678d9e6150
Kate Tester <katemorris@gmail.com>
2021-01-17 18:48:07 -0700

    Rubocop updates

--- spec/poros/image_spec.rb
+++ spec/poros/image_spec.rb
@@ -6,25 +6,26 @@ RSpec.describe Image do
   before :each do
     @location = 'Killeen, TX'
     @image_data = {
-      :id=>"50841175836",
-      :owner=>"53158134@N02",
-      :secret=>"94e3d109de",
-      :server=>"65535",
-      :farm=>66,
-      :title=>"N42RA (Rio Airways)",
-      :ispublic=>1,
-      :isfriend=>0,
-      :isfamily=>0}
+      id: '50841175836',
+      owner: '53158134@N02',
+      secret: '94e3d109de',
+      server: '65535',
+      farm: 66,
+      title: 'N42RA (Rio Airways)',
+      ispublic: 1,
+      isfriend: 0,
+      isfamily: 0
+    }
     @user_data = {
-      :id=>"53158134@N02",
-      :nsid=>"53158134@N02",
-      :pro_badge=>"standard",
-      :expire=>"0",
-      :username=>{:_content=>"Steelhead 2010"},
-      :realname=>{:_content=>"Reinhard Zinabold"},
-      :location=>{:_content=>"Ancaster/Ontario, Canada"},
-      :photosurl=>{:_content=>"https://www.flickr.com/photos/steelhead2010/"},
-      :profileurl=>{:_content=>"https://www.flickr.com/people/steelhead2010/"},
+      id: '53158134@N02',
+      nsid: '53158134@N02',
+      pro_badge: 'standard',
+      expire: '0',
+      username: { _content: 'Steelhead 2010' },
+      realname: { _content: 'Reinhard Zinabold' },
+      location: { _content: 'Ancaster/Ontario, Canada' },
+      photosurl: { _content: 'https://www.flickr.com/photos/steelhead2010/' },
+      profileurl: { _content: 'https://www.flickr.com/people/steelhead2010/' }
     }
     @image = Image.new(@image_data, @user_data, @location)
   end
