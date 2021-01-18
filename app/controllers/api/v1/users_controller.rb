d3d6793300633eb3b738e6771d06fa34e7592b49
Kate Tester <katemorris@gmail.com>
2021-01-17 18:46:36 -0700

    Adds apikey and error controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,18 +1,23 @@
 # frozen_string_literal: true
+require 'securerandom'
 
 module Api
   module V1
     class UsersController < ApplicationController
       def create
         json = JSON.parse(request.body.string, symbolize_names: true)
-        user = User.new(user_params(json))
-        render json: UserSerializer.new(user)
-      end
-
-      private
-
-      def user_params(data)
-        data.permit(:email, :password, :password_confirmation)
+        user = User.new({
+          email: json[:email],
+          password: json[:password],
+          password_confirmation: json[:password_confirmation],
+          api_key: SecureRandom.uuid
+          })
+        if user.save
+          render json: UserSerializer.new(user)
+        else
+          render json: { message: 'unsuccessful', error: user.errors.full_messages.to_sentence.to_s },
+                 status: :unprocessable_entity
+        end
       end
     end
   end
