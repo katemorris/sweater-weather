1c2741419cc00c95be3aacc941a7abef7b3a8e11
Kate Tester <katemorris@gmail.com>
2021-01-17 20:45:23 -0700

    Adds road trip method

--- spec/response_checkers.rb
+++ spec/response_checkers.rb
@@ -80,7 +80,7 @@ module ResponseCheckers
     expect(user[:data][:type]).to eq('user')
 
     expect(user[:data]).to have_key(:id)
-    expect(user[:data][:type]).to be_a String
+    expect(user[:data][:id]).to be_a String
 
     expect(user[:data]).to have_key(:attributes)
     expect(user[:data][:attributes]).to have_key(:email)
@@ -89,5 +89,37 @@ module ResponseCheckers
     expect(user[:data][:attributes]).to have_key(:api_key)
     expect(user[:data][:attributes]).to_not have_key(:password)
     expect(user[:data][:attributes]).to_not have_key(:password_confirmation)
+  end
+
+  def road_trip_checker(trip)
+    expect(trip).to be_a Hash
+    expect(trip).to have_key(:data)
+    expect(trip[:data]).to be_a Hash
+
+    expect(trip[:data]).to have_key(:type)
+    expect(trip[:data][:type]).to be_a String
+    expect(trip[:data][:type]).to eq('road_trip')
+
+    expect(trip[:data]).to have_key(:id)
+    expect(trip[:data][:id]).to be_a String
+    expect(trip[:data][:id]).to eq('null')
+
+    expect(trip[:data]).to have_key(:attributes)
+    expect(trip[:data][:attributes]).to have_key(:start_city)
+    expect(trip[:data][:attributes][:start_city]).to be_a String
+
+    expect(trip[:data][:attributes]).to have_key(:end_city)
+    expect(trip[:data][:attributes][:end_city]).to be_a String
+
+    expect(trip[:data][:attributes]).to have_key(:travel_time)
+    expect(trip[:data][:attributes][:travel_time]).to be_a String
+
+    expect(trip[:data][:attributes]).to have_key(:weather_at_eta)
+    expect(trip[:data][:attributes][:weather_at_eta]).to be_a Hash
+    expect(trip[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
+    expect(trip[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
+    expect(trip[:data][:attributes][:weather_at_eta][:temperature]).to be_a Float
+
+    expect(trip[:data][:attributes][:weather_at_eta]).to_not have_key(:sunrise)
   end
 end
