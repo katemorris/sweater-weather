822ae5b5ec23422b205485fbc13fd286eedb1513
Kate Tester <katemorris@gmail.com>
2021-01-18 12:03:20 -0700

    Adds sad path and edge case testing to forecast

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -4,8 +4,12 @@ module Api
   module V1
     class ForecastController < ApplicationController
       def show
-        forecast = ForecastFacade.by_location(params[:location])
-        render json: ForecastSerializer.new(forecast)
+        if params[:location].blank?
+          render json: { message: 'Please fill in a location.' }, status: :not_found
+        else
+          forecast = ForecastFacade.by_location(params[:location])
+          render json: ForecastSerializer.new(forecast)
+        end
       end
     end
   end
--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -13,4 +13,26 @@ RSpec.describe 'GET /api/v1/forecast?location=X', type: :request do
       forecast_get_response(forecast)
     end
   end
+
+  it 'given an incomplete location, it will return a forecast by guessing' do
+    VCR.use_cassette('arlington_incomplete_forecast') do
+      location = 'Arlington'
+      get api_v1_forecast_path, params: { location: location }
+
+      expect(response.status).to eq(200)
+      forecast = JSON.parse(response.body, symbolize_names: true)
+      forecast_get_response(forecast)
+    end
+  end
+
+  it 'given no location, no results are returned' do
+    VCR.use_cassette('no_location_forecast') do
+      location = ''
+      get api_v1_forecast_path, params: { location: location }
+
+      expect(response.status).to eq(404)
+      message = JSON.parse(response.body, symbolize_names: true)
+      expect(message[:message]).to eq('Please fill in a location.')
+    end
+  end
 end
