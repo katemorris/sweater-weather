606276ef34dd75c0e8105ed367bb8290cdfd519a
Kate Tester <katemorris@gmail.com>
2021-01-17 18:45:23 -0700

    Updates validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  validates :email, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true, email: true
   validates :api_key, uniqueness: true, presence: true
-  validates :password, presence: true, require: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 
   has_secure_password
 end
