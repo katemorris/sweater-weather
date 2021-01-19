647f8b9cd6faa56670e0cbd019c90bb459fd0f79
Kate Tester <katemorris@gmail.com>
2021-01-19 16:09:19 -0700

    Adds test for hourly weather method

--- spec/facades/road_trip_spec.rb
+++ spec/facades/road_trip_spec.rb
@@ -28,7 +28,6 @@ describe RoadTripFacade do
 
     it '.travel_in_seconds()' do
       time1 = '26:31:58'
-      time2 = '07:31:58'
       response1 = RoadTripFacade.travel_in_seconds(time1)
       expect(response1).to be_a Numeric
       expect(response1).to eq(95518)
@@ -37,6 +36,18 @@ describe RoadTripFacade do
       response2 = RoadTripFacade.travel_in_seconds(time2)
       expect(response2).to be_a Numeric
       expect(response2).to eq(27118)
+    end
+
+    it '.hourly_weather()' do
+      VCR.use_cassette('rt_facade_hourly_weather') do
+        destination = 'New Bern, NC'
+        time = '07:31:58'
+        response = RoadTripFacade.hourly_weather(destination, time)
+        expect(response).to be_a Hash
+        expect(response[:dt]).to be_an Integer
+        expect(response[:weather]).to be_an Array
+        expect(response[:weather].first).to have_key(:description)
+      end
     end
   end
 end
