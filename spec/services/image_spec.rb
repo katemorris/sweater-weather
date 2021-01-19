10f82c0ef6ced1f8b39ba4a61a2c4cc6d1640da8
Kate Tester <katemorris@gmail.com>
2021-01-19 16:32:25 -0700

    Beefs up tests for image service

--- spec/services/image_spec.rb
+++ spec/services/image_spec.rb
@@ -12,9 +12,13 @@ RSpec.describe ImageService do
         expect(response).to be_a Hash
 
         expect(response).to have_key(:owner)
+        expect(response[:owner]).to be_a String
         expect(response).to have_key(:id)
+        expect(response[:id]).to be_a String
         expect(response).to have_key(:secret)
+        expect(response[:secret]).to be_a String
         expect(response).to have_key(:server)
+        expect(response[:server]).to be_a String
         expect(response).to_not have_key(:photos)
         expect(response).to_not have_key(:photo)
       end
@@ -27,7 +31,13 @@ RSpec.describe ImageService do
 
         expect(response).to be_a Hash
         expect(response).to have_key(:username)
+        expect(response[:username]).to be_a Hash
+        expect(response[:username]).to have_key(:_content)
+        expect(response[:username][:_content]).to be_a String
         expect(response).to have_key(:photosurl)
+        expect(response[:photosurl]).to be_a Hash
+        expect(response[:photosurl]).to have_key(:_content)
+        expect(response[:photosurl][:_content]).to be_a String
         expect(response).to_not have_key(:person)
       end
     end
