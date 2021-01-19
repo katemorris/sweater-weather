32d5e804ac76c689f3e0212c072e3c933cbb3397
Kate Tester <katemorris@gmail.com>
2021-01-19 14:36:13 -0700

    breaks out weather service

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -2,7 +2,6 @@
 
 class WeatherService
   def self.forecast(coords, excludes = 'minutely,alerts', unit = 'imperial')
-    conn = Faraday.new('https://api.openweathermap.org')
     response = conn.get('/data/2.5/onecall') do |req|
       req.params['appid'] = ENV['OW_KEY']
       req.params['lat'] = coords[:lat]
@@ -11,5 +10,9 @@ class WeatherService
       req.params['units'] = unit
     end
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new('https://api.openweathermap.org')
   end
 end
