cb72bbcb5595c3f35a426d5a90e7e5afec6ba43c
Kate Tester <katemorris@gmail.com>
2021-01-19 09:25:57 -0700

    Adds controller for munchies

---
+++ app/controllers/api/v1/munchies_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class MunchiesController < ApplicationController
+      def create
+        data = MunchiesFacade.get_trip_data(params[:start], params[:end], params[:food])
+        render json: MunchiesSerializer.new(data)
+      end
+    end
+  end
+end
