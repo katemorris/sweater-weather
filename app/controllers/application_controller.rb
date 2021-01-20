1643e7d6b7301f66a846d62bf364930e31f5b5dd
Kate Tester <katemorris@gmail.com>
2021-01-19 18:58:24 -0700

    Rubocop updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-
   def location_not_found
     render json: { message: 'Please fill in a location.' }, status: :not_found
   end
